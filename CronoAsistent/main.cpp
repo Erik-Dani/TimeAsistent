f973cce1cd2edd307332630d10f8a7a446082b49
Erik-Dani <erik.chavacoh@udea.edu.co>
2023-04-07 20:56:28 -0500

    se hacen configuraciones en el codigo

--- CronoAsistent/main.cpp
+++ CronoAsistent/main.cpp
@@ -22,22 +22,22 @@ int main(void)
        //DinamikMemori(*Array[])
    }
    Dsalida_r.close();
-   char let='L';
-   for (int i = 0; i < 5; i++) {
-       for (int a = 0, car=0; Array[i][a]!='\0'; a++) {
-           if(Array[i][a]=='|') car++;
-           if(car==2){
-               for (int v = a+1;Array[v]!='|'; v++) {
-                   hora+=Array[v];
-                   if(let==Array[v]){
-                       cout<<"si hay clases"<<endl;
+//   char let='L';
+//   for (int i = 0; i < 5; i++) {
+//       for (int a = 0, car=0; Array[i][a]!='\0'; a++) {
+//           if(Array[i][a]=='|') car++;
+//           if(car==2){
+//               for (int v = a+1;Array[v]!='|'; v++) {
+//                   hora+=Array[v];
+//                   if(let==Array[v]){
+//                       cout<<"si hay clases"<<endl;
 
-                   }
-               }
+//                   }
+//               }
 
 
 
-           }
-       }
-   }
+//           }
+//       }
+//   }
 }
